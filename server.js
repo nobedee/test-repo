5301be75484ff0710042d3d24c9821948927fa3e
nobedee <87097541+nobedee@users.noreply.github.com>
2023-08-16 20:02:10 -0400

    reset

--- server.js
+++ server.js
@@ -1,17 +1,36 @@
-var http = require("http");
-var url = require("url");
-var fs = require("fs");
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
-http.createServer(function (req, res) {
- var q = url.parse(req.url, true);
- var filename = "." + "/index.html";
- fs.readFile(filename, function(err, data) {
-  if (err) {
-   res.writeHead(404, {"Content-Type" : "text/html"});
-   return res.end("404 Not Found");
+const server = http.createServer((req, res) => {
+  let filePath = path.join(__dirname, req.url);
+
+  // If the requested path is a directory, append 'index.html'
+  if (fs.statSync(filePath).isDirectory()) {
+    filePath = path.join(filePath, 'index.html');
   }
-  res.writeHead(200, {"Content-Type" : "text/html"});
-  res.write(data);
-  return res.end();
- });
-}).listen(3000);
+
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
+      res.end('File not found');
+    } else {
+      let contentType = 'text/html';
+
+      if (filePath.endsWith('.css')) {
+        contentType = 'text/css';
+      } else if (filePath.endsWith('.js')) {
+        contentType = 'application/javascript';
+      }
+
+      res.writeHead(200, { 'Content-Type': contentType });
+      res.end(data);
+    }
+  });
+});
+
+const PORT = 3000;
+
+server.listen(PORT, () => {
+  console.log(`Server is running at http://127.0.0.1:${PORT}`);
+});
