a75d4bb16cfba8c923a87c77e5c64953883f50be
nobedee <87097541+nobedee@users.noreply.github.com>
2023-09-13 19:59:44 -0400

    Add files via upload

--- data/ready/asciidoctor/config.sh
+++ data/ready/asciidoctor/config.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 # config
-# Config "config-variables.sh" for roffit test.
+# Config "config-variables.sh" for CHANGE test.
 
-_configReadyCommand="roffit"
+_configReadyCommand="asciidoctor"
 _configReadyTestRepoName="$_configReadyCommand-test"
-_configReadySourceUser="bagder"
-_configReadyOptions="declare -a _optionsToRun=('' '--bare')"
-_configReadyExtensions="declare -a _fileExtensions=('.1' '.3' '.8' '.1.in')"
-_configReadyRepoClones="declare -a _fullTestRepos=('nmap' 'nmap' 'libssh2' 'libssh2' 'curl' 'curl')"
-_configReadyUnique="_unqUser='curl'"
+_configReadySourceUser="asciidoctor"
+_configReadyOptions="declare -a _optionsToRun=('' '-o' '-D')"
+_configReadyExtensions="declare -a _fileExtensions=('.adoc' '.asciidoc' '.asc' '.ad' '.md')"
+_configReadyRepoClones="declare -a _fullTestRepos=('git-lfs' 'git-lfs' 'gofiber' 'fiber' 'asciidoctor' 'asciidoctor')"
+_configReadyUnique="_unqUser='asciidoctor'"
 
 sed -i "s|_COMMAND_TEST=\"command\"|_COMMAND_TEST=\"$_configReadyCommand\"|" config-variables.sh
 sed -i "s|_testResultRepo=\"command-test\"|_testResultRepo=\"$_configReadyTestRepoName\"|" config-variables.sh
 sed -i "s|_sourceUserName=\"source\"|_sourceUserName=\"$_configReadySourceUser\"|" config-variables.sh
 
-sed -i "s|_repoName=\"command-repo\"|_repoName=\"repo-name\"|" config-variables.sh
+sed -i "s|_repoName=\"command-repo\"\"|_repoName=\"repo-name\"|" config-variables.sh
 
 sed -i "s|declare -a _optionsToRun=(\"\")|$_configReadyOptions|" config-variables.sh
 sed -i "s|declare -a _fileExtensions=('.ext1' '.ext2' '.ext3')|$_configReadyExtensions|" config-variables.sh
 sed -i "s|declare -a _fullTestRepos=('userA' 'repoA' 'userB' 'repoB' 'userC' 'repoC')|$_configReadyRepoClones|" config-variables.sh
 sed -i "s|_unqUser=\"userC\"|$_configReadyUnique|" config-variables.sh
+
+# Not using command from test root
+sed -i "s|_toTestRoot=1|_toTestRoot=0|" config-variables.sh
